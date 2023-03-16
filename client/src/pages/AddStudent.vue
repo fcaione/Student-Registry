fe2a8bb7095ec7d11cf4f748c83ef9f84c965a5d
Clifton Lucas <cliftonlucas89@gmail.com>
2023-03-16 18:35:16 -0500

    merge

--- client/src/pages/AddStudent.vue
+++ client/src/pages/AddStudent.vue
@@ -24,8 +24,8 @@
 </template>
 
 <script>
-import axios from 'axios'
-import BASE_URL from '@/globals'
+// import axios from 'axios'
+// import BASE_URL from '@/globals'
 export default {
     name: 'AddStudent',
     data: () => ({
--- client/src/pages/SignIn.vue
+++ client/src/pages/SignIn.vue
@@ -1,56 +1,55 @@
 <template>
-<div>
-  
-  
-  <div class='SignInFormContainer'>
-  <h1>Sign In</h1>
-
-
-
-<div class="SignInForm">
-  <form  v-on:submit="handleSubmit">
-    <div>
-      <input @input="handleChange" placeholder="Email" :value="email" name="email" type="email" />
-  </div>
-
-   <div>
-    <input @input="handleChange" placeholder="Password" :value="password" name="password" type="password" />
-  </div>
-  </form>
-<div>
-  <h4>If you dont have an account, Sign Up Here!</h4>
-</div>
   <div>
-    <button @click="handleSubmit" :disabled="!email || !password">Log In</button>
-
-</div>
-</div>
-</div>
-</div>
-</template>
-
-<script>
-export default {
-  name: "SignIn",
-  data: () => ({
-    email: '',
-    password: ''
-  }),
-  methods: {
-    handleChange(e) {
-      this.$emit('handleFormChange', e.target.name, e.target.value)
+    
+    
+    <div class='SignInFormContainer'>
+    <h1>Sign In</h1>
+  
+  
+  
+  <div class="SignInForm">
+    <form  v-on:submit="handleSubmit">
+      <div>
+        <input @input="handleChange" placeholder="Email" :value="email" name="email" type="email" />
+    </div>
+     <div>
+      <input @input="handleChange" placeholder="Password" :value="password" name="password" type="password" />
+    </div>
+    </form>
+  <div>
+    <h4>If you dont have an account, Sign Up Here!</h4>
+  </div>
+    <div>
+      <button @click="handleSubmit" :disabled="!email || !password">Log In</button>
+  
+  </div>
+  </div>
+  </div>
+  </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "SignIn",
+    props: {
+      email: String,
+      password: String
     },
-    handleSubmit(e) {
-      e.preventDefault()
-            this.$emit('handleSubmit')
-
-    },
-    registerAccount(e){
-      e.preventDefault()
-            this.$emit('handleSubmit')
-
-      // this.$router.push('/signUp')
+    methods: {
+      handleChange(e) {
+        this.$emit('handleFormChange', e.target.name, e.target.value)
+      },
+      handleSubmit(e) {
+        e.preventDefault()
+           this.$emit('handleSubmit')
+  
+      }
+      // registerAccount(e){
+      //   e.preventDefault()
+      //         this.$emit('handleSubmit')
+  
+      //   // this.$router.push('/signUp')
+      // }
     }
   }
-}
-</script>
+  </script>
