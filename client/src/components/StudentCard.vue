4a487eaae8b56d6f8a834169843b922ad6dd7217
Clifton Lucas <cliftonlucas89@gmail.com>
2023-03-16 10:55:27 -0500

    updates to course card, studentcard

---
+++ client/src/components/StudentCard.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h3>Add A Course</h3>
+    <form @submit.prevent="handleSubmit">
+      <div>
+        <label for="name">Name:</label>
+        <input id="name" v-model="name" type="text" placeholder="Enter name name" required>
+      </div>
+      <div>
+        <label for="grade">Email:</label>
+        <input id="grade" v-model="email" type="email" placeholder="Enter course grade" required>
+      </div>
+      <div>
+        <label for="grade">Id:</label>
+        <input id="grade" v-model="id" type="text" placeholder="Enter course grade" required>
+      </div>
+      <div>
+        <label for="grade">Course:</label>
+        <input id="grade" v-model="course" type="text" placeholder="Enter course grade" required>
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        name: '',
+        grade: '',
+        email: '',
+        id: '',
+        course: ''
+      }
+    },
+    methods: {
+      handleSubmit() {
+        
+      }
+    }
+  }
+</script>
---
+++ client/src/pages/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+
+
+
+import axios from 'axios'
+
+
+
+  export default {
+    name: 'HomePage',
+    components:{
+   
+    },
+    data: () => ({
+
+    }),
+    mounted() {
+    
+      
+    },
+    methods: {
+
+      handleChange(e) {
+
+      },
+
+  
+    }
+
+  }
+</script>
