8e6b8c9fb69c3bee7adcff892699d29b1f19a170
Chikodi Merenu <cmerenu@gmail.com>
2023-03-16 15:03:39 -0500

    updated

--- client/src/pages/CourseDetails.vue
+++ client/src/pages/CourseDetails.vue
@@ -1,10 +1,22 @@
 <template>
-
+<div></div>
+<section class="details">
+    <div class="flex-row space">
+        <h3>{{ courseDetails.name }}</h3>
+        <p>{{ courseDetails.description }}</p>
+    </div>
+    <button @click="this.$router.push('/')"></button>
+    </section>
 </template>
 
 <script>
 
 export default {
-    name: 'CourseDetails'
+    name: 'CourseDetails',
+    data: () =>({
+        courseDetails: null
+    }),
+    mounted() {},
+    methods: {}
 }
 </script>
