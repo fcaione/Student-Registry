9ff1d1250999fe13e1e5fcb5db979c327daabdf1
Chikodi Merenu <cmerenu@gmail.com>
2023-03-16 12:32:28 -0500

    view/add  students/courses

--- client/src/pages/ViewStudents.vue
+++ client/src/pages/ViewStudents.vue
@@ -1,9 +1,20 @@
 <template>
-
+<div v-for="student in students" :key="student.id">
+    <StudentCard />
+</div>
 </template>
 
 <script>
+import StudentCard from '@/components/StudentCard.vue';
 export default {
-    name: 'ViewStudents'
+    name: 'ViewStudents',
+    components: {
+        StudentCard
+    },
+    data: () => ({
+        students: []
+    }),
+    mounted(){},
+    methods: {}
 }
 </script>
--- client/src/router.js
+++ client/src/router.js
@@ -4,13 +4,15 @@ import AboutPage from './pages/About'
 import AddStudent from './pages/AddStudent'
 import ViewCourses from './pages/ViewCourses'
 import ViewStudents from './pages/ViewStudents'
+import AddCourse from './pages/AddCourse'
 
 const routes = [
   { path: '/', component: HomePage, name: 'HomePage' },
   { path: '/about', component: AboutPage, name: 'AboutPage' },
   { path: '/students/add', component: AddStudent, name: 'AddStudent' },
   { path: '/students', component: ViewStudents, name: 'ViewStudents' },
-  { path: '/courses', component: ViewCourses, name: 'ViewCourses' }
+  { path: '/courses', component: ViewCourses, name: 'ViewCourses' },
+  { path: '/courses/add', component: AddCourse, name: 'AddCourse' }
 ]
 
 const router = createRouter({
