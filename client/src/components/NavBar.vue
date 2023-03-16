577b28d8297535a6592bc938bd446130bf933919
Chikodi Merenu <cmerenu@gmail.com>
2023-03-16 13:20:18 -0500

    signin/signUp

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,10 +1,13 @@
 <template>
 <nav>
     <router-link to="/">Home</router-link>
+    <router-link to="/signUp">SignUp</router-link>
+    <router-link to="/signIn">SignIn</router-link>
     <router-link to="/students">Students</router-link>
     <router-link to="/students/add">Add Students</router-link>
     <router-link to="/courses/add">Add Courses</router-link>
     <router-link to="/courses">Courses</router-link>
+    <router-link to="/details/course">Course Details</router-link>
     <router-link to="/about">About</router-link>
   </nav>
 </template>
---
+++ client/src/pages/SignUp.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+  <h1>Sign Up</h1>
+  <form @submit="handleSubmit">
+    <input placeholder="Email Address" name="email" type="email" :value="email" v-on:input="handleChange"/>
+    <input placeholder="Password" name="password" type="text" :value="password" v-on:input="handleChange"/>
+    <button>Sign Up!</button>
+  </form>
+</div>
+</template>
+
+<script>
+
+export default {
+    name: 'SignUp',
+    data: () => ({
+    email: '',
+    password: ''
+  }),
+  methods: {
+    handleChange(e) {
+      this[e.target.name] = e.target.value
+    },
+    handleSubmit(e) {
+      e.preventDefault()
+      this.email=''
+      this.password=''
+    },
+}
+}
+</script>
