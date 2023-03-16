6df95d3485ee886e7577aed9c9549735537daee1
Freddy Caione <fac299@nyu.edu>
2023-03-16 19:34:31 -0400

    add course works

--- client/src/pages/AddCourse.vue
+++ client/src/pages/AddCourse.vue
@@ -6,24 +6,30 @@
   <h3>Add A Course</h3>
 
   <div class="SignInForm">
-  <form @submit.prevent="handleSubmit">
-    <div>
-      <label for="name">Name:</label>
-    </div>
-    <div>
-      <input id="name" v-model="name" type="text" placeholder="Enter course name" required>
-    </div>
-    <div>
-      <label for="grade">Grade:</label>
-    </div>
-    <div>
-      <input id="grade" v-model="grade" type="text" placeholder="Enter course grade" required>
-    </div>
-    <div>
-      <button type="submit">Submit</button>
-    </div>
-  </form>
-</div> 
+				<form @submit.prevent="handleSubmit">
+					<label for="name">Name:</label>
+					<input
+						@input="handleChange"
+						id="name"
+						name="name"
+						type="text"
+						placeholder="Enter name"
+						:value="name"
+						required
+					/>
+					<label for="description">Description</label>
+					<input
+						@input="handleChange"
+						id="description"
+						name="description"
+						type="text"
+						placeholder="Enter description"
+						:value="description"
+						required
+					/>
+					<button type="submit">Submit</button>
+				</form>
+			</div>
   </div>
 
 </div>
@@ -33,18 +39,30 @@
 
 
 <script>
+import axios from 'axios'
+import BASE_URL from '@/globals'
+
   export default {
     name: 'AddCourse',
     data() {
       return {
         name: '',
-        grade: ''
+        description: ''
       }
     },
     methods: {
-      handleSubmit() {
-        
-      }
+      async handleSubmit() {
+			const res = await axios.post(`${BASE_URL}/courses/create/`, {
+				name: this.name,
+        description: this.description
+			})
+			console.log(res)
+			this.courses = res.data
+		},
+		handleChange(event) {
+			this[event.target.name] = event.target.value
+			console.log(this.name)
+		},
     }
   }
 </script>
