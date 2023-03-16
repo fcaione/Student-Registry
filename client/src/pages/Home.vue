0cd08865d398f67655c37f64ca87fec662e36c57
Clifton Lucas <cliftonlucas89@gmail.com>
2023-03-16 12:33:53 -0500

    merge

--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -1,38 +1,22 @@
 <template>
   <div>
-    <div class="search">
-      <form @submit="getSearchResults">
-        <input placeholder="Search Here" :value="searchQuery" name="search" type="text" v-on:input="handleChange" />
-        <button>Search</button>
-      </form>
-      <h2>Search Results</h2>
-      <section class="search-results container-grid" v-if="searched">
-      <div v-for="results in searchResults" :key="results.id">
-        <GameCard @click="selectGame(results.id)" :results="results"/>
-      </div>
-    </section>
-    </div>
+    
   </div>
 </template>
 
+
 <script>
+import About from ''
+
 export default {
     name: 'HomePage',
     components:{},
     data: () => ({
-      searchQuery: '',
-      searchResults: [],
-      searched: false
+      
     }),
     mounted() {},
     methods: {
-      async getSearchResults(e) {
-        e.preventDefault()
-        const res = await axios.get ()
-        this.searchResults= res.data.results
-        this.searched = true
-        this.searchQuery = ''
-    }
+    
   }
   }
 </script>
---
+++ client/src/pages/ViewStudents.vue
@@ -0,0 +1,66 @@
+<template>
+  <div className="container-grid">
+
+
+
+    <div className="car">
+      <h1 className="Title"> Car Collection </h1>
+      <div className="generalPage">
+        <div className="Car-Card">
+          <div className="column1">
+            {carList.map(
+              (cars, index) =>
+                index % 2 == 0 && (
+                  <div>
+                    <Link to={`/CarDetails/${cars.id}`}>
+                      <img
+                        src={cars.image}
+                        alt="car image"
+                        className="homepic"
+                      />
+                    </Link>
+                    <p className="make">
+                      {cars.make} {cars.model}
+                    </p>
+                  </div>
+                )
+            )}
+          </div>
+          <div className="column2">
+            {carList.map(
+              (cars, index) =>
+                index % 2 != 0 && (
+                  <div className="Car-Card2">
+                    <Link to={`/CarDetails/${cars.id}`}>
+                      <img src={cars.image} alt="car" className="homepic" />
+                    </Link>
+                    <p className="make">
+                      {cars.make} {cars.model}
+                    </p>
+                  </div>
+                )
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ViewStudents',
+    data: () => ({
+      games: []
+    }),
+
+    mounted() {},
+    methods: {
+      async getAllStudents() {
+       
+      }
+    }
+  }
+</script>
